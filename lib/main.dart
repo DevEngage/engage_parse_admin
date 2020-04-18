3ee677f0db1a8a3ea1b6653dd3802c052a7e556e
James Brinkerhoff <th3brink@gmail.com>
2020-04-18 12:16:46 -0600

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,11 @@ import 'package:engage_parse_admin/models/game_model.dart';
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
 
-// const String PARSE_APP_ID = '';
-// const String PARSE_APP_URL = '';
-// const String MASTER_KEY = '';
-// const String CLIENT_KEY = '';
-// const String LIVE_QUERY_URL = '';
-
-const String PARSE_APP_ID = 'LkmOVjo3u8bdEyXCkIBKY4kDOs1q6tiQLzUXXZTZ';
-const String PARSE_APP_URL = 'https://parseapi.back4app.com';
-const String MASTER_KEY = 'ZHH5Ez7W8qtYsdG3y6fS2ltInSD28cgxH3E4m9By';
-const String CLIENT_KEY = 'WU3oevS75xMv7dGcr8rghkWoci33GaP8lGZgNbbc';
-const String LIVE_QUERY_URL = 'wss://trongdth.back4app.io';
+const String PARSE_APP_ID = '';
+const String PARSE_APP_URL = '';
+const String MASTER_KEY = '';
+const String CLIENT_KEY = '';
+const String LIVE_QUERY_URL = '';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
